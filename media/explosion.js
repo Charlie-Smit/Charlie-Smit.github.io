16f496831daa9bc0ca2c334e56c99a215811cb28
Charlie-Smit <Kadee.edge@gmail.com>
2025-02-07 22:46:56 -0500

    don't look at explosions.

---
+++ media/explosion.js
@@ -0,0 +1,23 @@
+$(document).on('click', function(event) {
+    var clickX = event.pageX;
+    var clickY = event.pageY;
+    
+    // Create a new image element for each click
+    var $explosion = $('<img>', {
+        src: 'img/explosion.gif', // Replace with your explosion GIF URL
+        alt: 'Explosion!',
+        class: 'explosion',
+        css: {
+            left: clickX - 50 + 'px', // Center explosion at the click point
+            top: clickY - 70 + 'px' }
+        });
+
+    $('body').append($explosion);
+
+    // Hide the explosion after 1.6 seconds
+    setTimeout(function() {
+        $explosion.fadeOut(function() {
+            $(this).remove();
+        });
+    }, 1600); // 1600 milliseconds = 1.6 seconds
+});
