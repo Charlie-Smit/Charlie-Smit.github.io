b658084ef43daaf256607f147b6066d32f619623
Charlie-Smit <Kadee.edge@gmail.com>
2025-02-07 22:57:53 -0500

    mebbe

--- media/explosion.js
+++ media/explosion.js
@@ -1,26 +1,32 @@
-$(document).on('click', function(event) {
-    var clickX = event.pageX;
-    var clickY = event.pageY;
-    
-    // Create a new image element for each click
-    var $explosion = $('<img>', {
-        src: 'img/explosion.gif', // Replace with your explosion GIF URL
-        alt: 'Explosion!',
-        class: 'explosion'
-    }).css({
-        left: clickX - 50 + 'px', // Center explosion at the click point
-        top: clickY - 70 + 'px'
-    });
-    console.log('explode!');
-    $('body').append($explosion);
-
-    // Hide the explosion after 1.6 seconds
-    setTimeout(function() {
-        $explosion.fadeOut(function() {
-            $(this).remove();
-            console.log('removing!');
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $(document).on('click', function(event) {
+        var clickX = event.pageX;
+        var clickY = event.pageY;
+        
+        // Create a new image element for each click
+        var $explosion = $('<img>', {
+            src: 'img/explosion.gif', // Replace with your explosion GIF URL
+            alt: 'Explosion!',
+            class: 'explosion'
+        }).css({
+            left: clickX - 50 + 'px', // Center explosion at the click point
+            top: clickY - 70 + 'px'
         });
-    }, 1600); // 1600 milliseconds = 1.6 seconds
+        console.log('explode!');
+        $('body').append($explosion);
+
+        // Hide the explosion after 1.6 seconds
+        setTimeout(function() {
+            $explosion.fadeOut(function() {
+                $(this).remove();
+                console.log('removing!');
+            });
+        }, 1600); // 1600 milliseconds = 1.6 seconds
+    });
+
+    console.log('event registered');
+
 });
 
-console.log('event registered');
+console.log('script registered');
