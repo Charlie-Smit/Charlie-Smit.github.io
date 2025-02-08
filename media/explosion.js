fbbf7d5c9d8d149c0e40c22424d598cd7cfc6549
Charlie-Smit <Kadee.edge@gmail.com>
2025-02-07 22:52:00 -0500

    debuggg

--- media/explosion.js
+++ media/explosion.js
@@ -6,18 +6,21 @@ $(document).on('click', function(event) {
     var $explosion = $('<img>', {
         src: 'img/explosion.gif', // Replace with your explosion GIF URL
         alt: 'Explosion!',
-        class: 'explosion',
-        css: {
-            left: clickX - 50 + 'px', // Center explosion at the click point
-            top: clickY - 70 + 'px' }
-        });
-
+        class: 'explosion'
+    }).css({
+        left: clickX - 50 + 'px', // Center explosion at the click point
+        top: clickY - 70 + 'px'
+    });
+    console.log('explode!');
     $('body').append($explosion);
 
     // Hide the explosion after 1.6 seconds
     setTimeout(function() {
         $explosion.fadeOut(function() {
             $(this).remove();
+            console.log('removing!');
         });
     }, 1600); // 1600 milliseconds = 1.6 seconds
 });
+
+console.log('event registered');
